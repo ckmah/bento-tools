d0cc552ca429145894564afece5017fe630bcb2c
Clarence K Mah <ckmah@tscc-login1.sdsc.edu>
2020-05-08 00:29:17 -0700

    implement remaining features; add global features for expensive calculations

--- bento/preprocessing/_preprocessing.py
+++ bento/preprocessing/_preprocessing.py
@@ -1,31 +1,44 @@
+import sys
+
+import geopandas
 import numpy as np
 import pandas as pd
-import geopandas
 from astropy.stats import RipleysKEstimator
+import matplotlib.path as mplPath
+from scipy.spatial import distance, distance_matrix
 from scipy.stats import spearmanr
 from scipy.stats.mstats import zscore
-from scipy.spatial import distance, distance_matrix
-from loguru import logger
+from sklearn.metrics import mean_squared_error
+from pandarallel import pandarallel
 from tqdm import tqdm
 
+pandarallel.initialize(nb_workers=4)
+
+# Global computations for memoization
+_progress_bar = None
+_cell_morph_masks = {}
 
 def prepare_features(data):
     """
     """
-    num_genes = len(data['points']['gene'].unique())
-    logger.info(f'Preparing features for {num_genes} genes...')
+    global _progress_bar
+    _progress_bar = tqdm(
+        zip(data['cell_id'], data['cell'], data['nucleus']), total=len(data['cell']))
+    _progress_bar.set_description('Preparing features...')
 
     features = []
-    for id, cell_mask, nucleus_mask in tqdm(zip(data['cell_id'], data['cell'], data['nucleus']), total=len(data['cell'])):
+    for id, cell_mask, nucleus_mask in _progress_bar:
         # Select points in cell
         cell_points = geopandas.clip(data['points'], cell_mask)
+        _cell_morph_masks = {}
 
         # Prepare features for each gene
-        cell_features = cell_points.groupby('gene').apply(
+        cell_features = cell_points.groupby('gene').parallel_apply(
             lambda gene_points: _prepare_features(gene_points, cell_mask, nucleus_mask))
 
         cell_features['cell_id'] = id
         features.append(cell_features)
+
 
     features = pd.concat(features)
 
@@ -41,6 +54,11 @@ def _prepare_features(points, cell_mask, nucleus_mask):
     cell_features = _calc_ripley_features(points, cell_mask)
     cell_features = cell_features.append(
         _calc_norm_distance_quantile_features(points, cell_mask, nucleus_mask))
+    cell_features = cell_features.append(
+        _calc_morph_enrichment(points, cell_mask))
+    cell_features = cell_features.append(
+        _calc_nuclear_fraction(points, cell_mask, nucleus_mask))
+    cell_features = cell_features.append(_calc_indexes(points, cell_mask))
 
     return cell_features
 
@@ -58,6 +76,9 @@ def _calc_ripley_features(points, mask):
         monotony : float
         l_4 : float
     """
+    global _progress_bar
+    _progress_bar.set_description('Computing Ripley features...')
+
     ripley_norm, radii = _ripley(points, mask)
 
     # Max value of the L-function
@@ -84,23 +105,31 @@ def _calc_ripley_features(points, mask):
 
 def _ripley(points, mask, radii=None):
     """
-    Calculate estimation for Ripley K function
+    Calculate estimation for Ripley H function.
+
+    Parameters
+    ----------
+    points : GeoDataFrame
+    mask : shapely.Polygon
+    radii : float list
+
+    Returns
+    -------
+    list
+        ripley l function
+    radii
     """
-    estimator_params = dict(area=mask.area,
-                            x_min=points.x.min(), x_max=points.x.max(),
-                            y_min=points.y.min(), y_max=points.y.max())
-    estimator = RipleysKEstimator(**estimator_params)
+    estimator = RipleysKEstimator(area=mask.area,
+                                  x_min=points.x.min(), x_max=points.x.max(),
+                                  y_min=points.y.min(), y_max=points.y.max())
 
     # https://docs.astropy.org/en/stable/api/astropy.stats.RipleysKEstimator.html#astropy.stats.RipleysKEstimator
     if radii is None:
-        radii = np.linspace(1, np.sqrt(
-            estimator_params['area'] / 2) ** 0.5, 200)
-    ripley = estimator(data=points[['x', 'y']], radii=radii, mode='none')
+        radii = np.linspace(1, np.sqrt(mask.area / 2) ** 0.5, 200)
+    ripley = estimator.Hfunction(
+        data=points[['x', 'y']], radii=radii, mode='none')
 
-    # L-function normalization (see supp info: https://www.nature.com/articles/s41467-018-06868-w)
-    ripley_norm = np.sqrt(ripley / np.pi) - radii
-
-    return ripley_norm, radii
+    return ripley, radii
 
 
 def _calc_norm_distance_quantile_features(points, cell_mask, nucleus_mask):
@@ -119,8 +148,8 @@ def _calc_norm_distance_quantile_features(points, cell_mask, nucleus_mask):
     -------
     pd.Series
     """
-    # TODO remove later when this method is called in prepare_features
-    points = geopandas.clip(points, cell_mask)
+    global _progress_bar
+    _progress_bar.set_description('Computing distance features...')
 
     # Calculate normalized distances from points to centroids
     cell_centroid_dist = _calc_norm_dist_to_centroid(points, cell_mask)
@@ -175,13 +204,70 @@ def _calc_norm_dist_to_mask(points, mask):
     return observed_distance / expected_distance
 
 
-def _calc_polarization_index():
-    pass
+def _calc_nuclear_fraction(points, cell_mask, nucleus_mask):
+    total_count = len(points)
+    nuclear_count = len(geopandas.clip(points, nucleus_mask))
+    ratio = float(nuclear_count) / total_count
+    return pd.Series(ratio, index=['nuclear_fraction'])
 
 
-def _calc_dispersion_index():
-    pass
+def _calc_indexes(points, mask):
+    """Calculate polarization index, dispersion index, and peripheral index.
+    """
+    global _progress_bar
+    _progress_bar.set_description('Computing index features...')
+
+    point_centroid = [points[['x', 'y']].mean().values]
+    cell_centroid = [np.array(mask.centroid)]
+    offset = distance.cdist(point_centroid, cell_centroid)[0][0]
+
+    mask_grid = _poly2grid(mask)
+    cell_centroid_array = np.tile(cell_centroid, len(mask_grid)).reshape(-1, 2)
+
+    radius_of_gyration = mean_squared_error(mask_grid, cell_centroid_array)
+    polarization_index = offset / radius_of_gyration
+
+    return pd.Series([polarization_index], index=['polarization_index'])
 
 
-def _calc_morph_enrichment():
-    pass
+def _poly2grid(polygon):
+    minx, miny, maxx, maxy = polygon.bounds
+    x, y = np.meshgrid(np.linspace(minx, maxx, num=100),
+                       np.linspace(miny, maxy, num=100))
+    x = x.flatten()
+    y = y.flatten()
+    xy = np.array([x, y]).T
+    polygon_path = mplPath.Path(np.array(polygon.exterior.xy).T)
+    polygon_mask = polygon_path.contains_points(xy)
+    xy = xy[polygon_mask]
+
+    return xy
+
+
+def _calc_morph_enrichment(points, mask):
+    global _progress_bar
+    _progress_bar.set_description('Computing morphological features...')
+
+    # Mean radius size
+    mean_radius = distance.cdist(np.array(mask.centroid).reshape(1, 2),
+                                 np.array(mask.exterior.xy).T)
+    mean_radius = abs(mean_radius).mean()
+    total_points = len(points)
+
+    global _cell_morph_masks
+
+    enrichment = {}
+    proportions = [.1, .25]
+    for p in proportions:
+        d = mean_radius * p
+
+        p_str = str(p)
+        if p_str not in _cell_morph_masks:
+            _cell_morph_masks[p_str] = mask.buffer(distance=-d).buffer(distance=d)
+
+        n_points = len(geopandas.clip(points, _cell_morph_masks[p_str]))
+
+        stat = np.float(n_points) / total_points
+        enrichment[f'morph_enrichment_{p}'] = stat
+
+    return pd.Series(enrichment)
