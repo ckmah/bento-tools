2b5840d7a9bc291a37605a9189154d658e241102
Clarence Mah <ckmah@ucsd.edu>
2021-02-22 13:38:15 -0800

    cell morph features

--- bento/tools/__init__.py
+++ bento/tools/__init__.py
@@ -1,3 +1,4 @@
-from ._tools import pca, subsample, umap, score_genes_cell_cycle, spots_diff
+from ._tools import pca, subsample_points, umap, score_genes_cell_cycle, spots_diff, _map_to_obs
+from ._cell_features import cell_aspect_ratio, cell_area
 from ._features import prepare_features, list_features, get_feature, rasterize_cells
 from ._spots import detect_spots
---
+++ bento/tools/_cell_features.py
@@ -0,0 +1,36 @@
+from shapely.geometry import Point
+
+def cell_aspect_ratio(data, copy=False):
+
+    adata = data.copy() if copy else data
+
+    def _aspect_ratio(poly):
+        # get coordinates of min bounding box vertices around polygon
+        x, y = poly.minimum_rotated_rectangle.exterior.coords.xy
+
+        # get length of bound box sides
+        edge_length = (Point(x[0], y[0]).distance(Point(x[1], y[1])), Point(x[1], y[1]).distance(Point(x[2], y[2])))
+
+        # length = longest side, width = shortest side
+        length, width = max(edge_length), min(edge_length)
+
+        # return long / short ratio
+        return length / width
+
+    ar = adata.uns['masks']['cell']['geometry'].apply(lambda poly: _aspect_ratio(poly))
+    ar = ar.reindex(adata.obs.index)
+    adata.obs['aspect-ratio'] = ar
+
+    return adata if copy else None
+
+
+def cell_area(data, copy=False):
+    adata = data.copy() if copy else data
+
+    # Calculate pixel-wise area 
+    # TODO: unit scale?
+    area = adata.uns['masks']['cell']['geometry'].area
+    area = area.reindex(adata.obs.index)
+    adata.obs['area'] = area
+
+    return adata if copy else None
