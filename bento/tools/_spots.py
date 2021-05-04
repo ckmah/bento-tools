e7760ce8238617485750d902f09b2fb71d6b4481
Clarence Mah <ckmah@ucsd.edu>
2021-05-03 19:39:34 -0700

    allow alternate models for pattern detection

--- bento/tools/_spots.py
+++ bento/tools/_spots.py
@@ -13,7 +13,7 @@ from skorch.callbacks import Checkpoint
 from sklearn.preprocessing import OneHotEncoder
 
 
-def detect_spots(data, imagedir, device="auto", copy=False):
+def detect_spots(data, imagedir, device="auto", model="pattern", copy=False):
     """
     Detect and label localization patterns.
     TODO change data to be iterable compatible with skorch.predict_proba
@@ -23,7 +23,7 @@ def detect_spots(data, imagedir, device="auto", copy=False):
     data : [type]
         [description]
     imagedir : str
-        Folder for bento rasterized images.
+        Folder for rasterized images.
     Returns
     -------
     [type]
@@ -35,18 +35,17 @@ def detect_spots(data, imagedir, device="auto", copy=False):
     if device == "auto":
         device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    model_dir = "/".join(bento.__file__.split("/")[:-1]) + "/models/spots"
+    model_dir = "/".join(bento.__file__.split("/")[:-1]) + "/models/spots/" + model
 
     model_params = pickle.load(open(f"{model_dir}/params.p", "rb"))
 
-    # Load ensemble model
-    # Reload model with temperature and softmax
-    net = NeuralNetClassifier(module=SpotsModule(**model_params), device=device)
-
     # Load model
+    modules = dict(pattern=SpotsModule, five_pattern=FiveSpotsModule)
+    module = modules[model](**model_params)
+
+    net = NeuralNetClassifier(module=module, device=device)
     net.initialize()
     net.load_params(checkpoint=Checkpoint(dirname=f"{model_dir}"))
-    # TODO install cudatoolkit. pip?
 
     dataset = datasets.ImageFolder(
         imagedir,
@@ -56,13 +55,26 @@ def detect_spots(data, imagedir, device="auto", copy=False):
     # Sample by n_classes
     pred_prob = net.predict_proba(dataset)
 
-    # pred = pred_prob >= 0.5
-    # pred = pred.astype(int)
+    if isinstance(module, FiveSpotsModule):
+        label_names = [
+            "cell edge",
+            "nuclear edge",
+            "perinuclear",
+            "protrusions",
+            "random",
+        ]
+    else:
+        label_names = [
+            "cell edge",
+            "foci",
+            "nuclear edge",
+            "perinuclear",
+            "protrusions",
+            "random",
+        ]
 
     encoder = OneHotEncoder(handle_unknown="ignore").fit(
-        np.array(["cell2D", "cellext", "foci", "nuc2D", "polarized", "random"]).reshape(
-            -1, 1
-        )
+        np.array(label_names).reshape(-1, 1)
     )
 
     # Cell gene names
@@ -74,11 +86,31 @@ def detect_spots(data, imagedir, device="auto", copy=False):
 
     pattern_labels = (
         spots_pred_long.pivot(index="cell", columns="gene", values="label")
-        .fillna('none')
-        .reindex(index=adata.obs_names, columns=adata.var_names, fill_value='none')
+        .fillna("none")
+        .reindex(index=adata.obs_names, columns=adata.var_names, fill_value="none")
     )
 
-    adata.layers["pattern"] = pattern_labels
+    adata.layers[model] = pattern_labels
+
+    # Annotate points with pattern labels
+    plabels_long = pattern_labels.reset_index().melt(id_vars="cell")
+    plabels_long["cell"] = plabels_long["cell"].map(
+        {v: k for k, v in adata.uns["point_cell_index"].items()}
+    )
+    plabels_long["gene"] = plabels_long["gene"].map(
+        {v: k for k, v in adata.uns["point_gene_index"].items()}
+    )
+
+    plabels_long = plabels_long.rename({"value": model}, axis=1)
+
+    if model in adata.uns["points"].columns:
+        adata.uns["points"].drop([model], axis=1, inplace=True)
+
+    adata.uns["points"] = adata.uns["points"].merge(
+        plabels_long, how="left", on=["cell", "gene"]
+    )
+
+    adata.uns["points"][model] = adata.uns["points"][model].astype("category")
 
     return adata if copy else None
 
@@ -104,6 +136,7 @@ class DataReshape:
     def transform(self, X):
         X = X.reshape(X.shape[0], 1, 64, 64)
         return X
+
 
 class SpotsModule(nn.Module):
     def __init__(
@@ -160,6 +193,72 @@ class SpotsModule(nn.Module):
             in_features = out_features
 
         fc_layers.append(nn.Linear(in_features, 6))
+
+        self.model = nn.Sequential(*[*conv_layers, *fc_layers])
+
+    def forward(self, x):
+        x = self.model(x)
+
+        x = F.softmax(x, dim=-1)
+
+        return x
+
+
+class FiveSpotsModule(nn.Module):
+    def __init__(
+        self,
+        n_conv_layers,
+        in_dim,
+        out_channels,
+        kernel_size,
+        f_units_l0,
+        f_units_l1,
+    ) -> None:
+        super().__init__()
+        conv_layers = []
+
+        in_channels = 1
+        in_dim = in_dim
+
+        # Stack (convolutions + batchnorm + activation) + maxpool
+        for i in range(n_conv_layers):
+            conv_layers.append(
+                nn.Conv2d(
+                    in_channels=in_channels,
+                    out_channels=out_channels,
+                    kernel_size=kernel_size,
+                )
+            )
+            conv_layers.append(nn.BatchNorm2d(out_channels))
+            conv_layers.append(nn.ReLU())
+
+            # Compute convolved output dimensions
+            in_dim = get_conv_dim(
+                in_dim, padding=0, dilation=1, kernel_size=kernel_size, stride=1
+            )
+
+            in_channels = out_channels
+            out_channels *= 2
+
+        out_channels = int(out_channels / 2)
+
+        conv_layers.append(nn.MaxPool2d(2, 2))
+        in_dim = int(in_dim / 2)
+
+        # We optimize the number of layers, hidden units and dropout ratio in each layer.
+        fc_layers = [nn.Flatten()]
+
+        # Compute flatten size
+        in_features = out_channels * in_dim * in_dim
+        for i in [f_units_l0, f_units_l1]:
+            out_features = i
+            fc_layers.append(nn.Linear(in_features, out_features))
+            fc_layers.append(nn.BatchNorm1d(out_features))
+            fc_layers.append(nn.ReLU())
+
+            in_features = out_features
+
+        fc_layers.append(nn.Linear(in_features, 5))
 
         self.model = nn.Sequential(*[*conv_layers, *fc_layers])
 
