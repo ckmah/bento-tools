104cfc32c962259075bdb08e2f994afbe7aec49a
Clarence Mah <clarence.iyan@gmail.com>
2021-12-10 03:50:30 +0000

    stop importing removed functions

--- bento/tools/__init__.py
+++ bento/tools/__init__.py
@@ -1,7 +1,18 @@
 from ._pattern_stats import pattern_diff, pattern_stats
 
-from ._cell_features import cell_area, cell_aspect_ratio, cell_radius, cell_perimeter, is_nuclear, cell_morph_open, nucleus_area, nucleus_aspect_ratio, nucleus_area_ratio, nucleus_offset
-from ._features import coloc_cluster_genes, coloc_sim, gene_leiden, rasterize_cells
+from ._cell_features import (
+    cell_area,
+    cell_aspect_ratio,
+    cell_radius,
+    cell_perimeter,
+    is_nuclear,
+    cell_morph_open,
+    nucleus_area,
+    nucleus_aspect_ratio,
+    nucleus_area_ratio,
+    nucleus_offset,
+)
+from ._features import coloc_cluster_genes, coloc_sim, gene_leiden
 from ._locfish_features import (
     distance_features,
     moment_stats,
@@ -9,8 +20,19 @@ from ._locfish_features import (
     nuclear_fraction,
     ripley_features,
 )
-from ._pattern_models import predict_patterns, intracellular_patterns
-from ._sample_features import ShapeProximity, ShapeAsymmetry, Ripley, ShapeEnrichment, PointDispersion, ShapeDispersion
+from ._pattern_models import intracellular_patterns
+from ._sample_features import (
+    ShapeProximity,
+    ShapeAsymmetry,
+    Ripley,
+    ShapeEnrichment,
+    PointDispersion,
+    ShapeDispersion,
+)
 from ._shapes import inner_edge, outer_edge
-from ._tensor_tools import to_tensor, decompose_tensor, select_tensor_rank, assign_factors
-
+from ._tensor_tools import (
+    to_tensor,
+    decompose_tensor,
+    select_tensor_rank,
+    assign_factors,
+)
