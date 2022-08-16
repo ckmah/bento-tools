091617c958eefa524001bef9fa2087e57968f00e
Clarence Mah <clarence.iyan@gmail.com>
2022-08-16 13:37:30 -0700

    simplify tensor decomposition for signature analysis

--- bento/_utils.py
+++ bento/_utils.py
@@ -7,7 +7,6 @@ import seaborn as sns
 
 PATTERN_NAMES = ["cell_edge", "cytoplasmic", "none", "nuclear", "nuclear_edge"]
 PATTERN_PROBS = [f"{p}_p" for p in PATTERN_NAMES]
-TENSOR_DIM_NAMES = ["layers", "cells", "genes"]
 
 # Colors correspond to order of PATTERN_NAMES: cyan, blue, gray, orange, red
 PATTERN_COLORS = ['#17becf', '#1f77b4', '#7f7f7f', '#ff7f0e', '#d62728']
--- bento/tools/_tensor_tools.py
+++ bento/tools/_tensor_tools.py
@@ -5,12 +5,16 @@ import pandas as pd
 import seaborn as sns
 from scipy.stats import zscore
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
+import tensorly as tl
+from tensorly.decomposition import non_negative_parafac
+from tensorly.metrics.regression import RMSE
+from tqdm.auto import tqdm
 
-from .._utils import track, PATTERN_NAMES, TENSOR_DIM_NAMES
+from .._utils import track, PATTERN_PROBS
 
 
 @track
-def to_tensor(data, layers, scale=True, copy=False):
+def to_tensor(data, layers, scale=False, copy=False):
     """
     Generate tensor from data where dimensions are (layers, cells, genes).
 
@@ -30,113 +34,42 @@ def to_tensor(data, layers, scale=True, copy=False):
 
         `uns['tensor']` : np.ndarray
             3D numpy array of shape (len(layers), adata.n_obs, adata.n_vars)
-
-        `uns['tensor_labels'] : dict
-            Element labels across each dimension. Keys are dimension names (layers, cells, genes),
-            values are lists of str
-
     """
     adata = data.copy() if copy else data
 
-    cells = data.obs_names.tolist()
+    cells = adata.obs_names.tolist()
     genes = adata.var_names.tolist()
 
     # Build tensor from specified layers
     tensor = []
     for l in layers:
         tensor.append(adata.to_df(l).values)
-        
+
     # Save tensor values
     tensor = np.array(tensor)
-    tensor = zscore(tensor, axis=0)
-    adata.uns["tensor"] = np.array(tensor)
 
-    # Save tensor dimension indexes
-    adata.uns["tensor_labels"] = dict(zip(TENSOR_DIM_NAMES, [layers, cells, genes]))
+    # Z scale across cells for each layer
+    if scale:
+        for i, layer in enumerate(tensor):
+            tensor[i] = zscore(layer, axis=1, nan_policy="omit")
+
+    adata.uns["tensor"] = np.array(tensor)
 
     return adata
 
 
-def select_tensor_rank(data, layers, upper_rank=5, runs=3, device="auto", random_state=888, copy=False):
-    """Perform `bento.tl.decompose_tensor()` up to rank `upper_rank` repeating each decomposition
-    `runs` times to compute a 95% confidence interval, plotting reconstruction error for each rank.
-
-    Parameters
-    ----------
-    data : AnnData
-        Spatial formatted AnnData.
-    layers : list of str
-        Keys in data.layers to build tensor.
-    upper_rank : int
-        Maximum rank to perform decomposition, by default 10.
-    runs : int
-        Number of times to run decomposition for calculating the confidence interval, by default 5.
-    device : str, optional
-        Type of device to use, valid options include `cpu`, `gpu`, and `auto`, by default `auto`.
-        Option `auto` prefers `gpu` over `cpu` if available.
-    random_state : int, optional
-        Random seed used for reproducibility, by default 888
-    copy : bool
-        Return a copy of `data` instead of writing to data, by default False.
-
-    Returns
-    -------
-    adata : anndata.AnnData
-        Returns `adata` if `copy=True`, otherwise adds fields to `data`:
-
-        `uns['tensor']` : np.ndarray
-            3D numpy array of shape (len(layers), adata.n_obs, adata.n_vars)
-
-        `uns['tensor_labels'] : dict
-            Element labels across each dimension. Keys are dimension names (layers, cells, genes),
-            values are lists of str
-    """
-    adata = data.copy() if copy else data
-
-    to_tensor(adata, layers=layers, mask=True)
-    tensor_c2c = init_c2c_tensor(adata, device=device)
-
-    fig, error = tensor_c2c.elbow_rank_selection(
-        upper_rank=upper_rank,
-        runs=runs,
-        init="random",
-        automatic_elbow=True,
-        random_state=random_state,
-    )
-
-    plt.tight_layout()
-
-
-def lp_signatures(data, rank, device="auto", random_state=888, copy=False):
-    """Calculate localization signatures by performing tensor decomposition on the dataset tensor.
-        Wrapper for `bento.tl.decompose_tensor()`.
-
-    Parameters
-    ----------
-    data : AnnData
-        Spatial formatted AnnData.
-    rank : int
-        Rank to perform decomposition.
-    device : str, optional
-        Type of device to use, valid options include `cpu`, `gpu`, and `auto`, by default `auto`.
-        Option `auto` prefers `gpu` over `cpu` if available.
-    random_state : int, optional
-        Random seed used for reproducibility, by default 888.
-    copy : bool
-        Return a copy of `data` instead of writing to data, by default False.
-
-    Returns
-    -------
-    _type_
-        _description_
-    """
-    return decompose_tensor(
-        data, PATTERN_NAMES, rank, device=device, random_state=random_state, copy=copy
-    )
-
-
 @track
-def decompose_tensor(data, layers, rank, device="auto", random_state=888, copy=False):
+def signatures(
+    data,
+    layers,
+    ranks,
+    nruns=3,
+    fillna=0,
+    scale=True,
+    device="auto",
+    random_state=888,
+    copy=False,
+):
     """Perform tensor decomposition on the 3-dimensional tensor built from `[cell, gene, layers]`.
 
     Parameters
@@ -161,105 +94,114 @@ def decompose_tensor(data, layers, rank, device="auto", random_state=888, copy=F
     """
     adata = data.copy() if copy else data
 
-    to_tensor(data, layers=layers, mask=True)
-    tensor_c2c = init_c2c_tensor(data, device=device)
+    to_tensor(adata, layers=layers, scale=scale)
+    tensor = adata.uns["tensor"].copy()
 
-    tensor_c2c.compute_tensor_factorization(
-        rank=rank,
-        init="random",
-        random_state=random_state,
+    # Replace nans with 0
+    if fillna == 0:
+        tensor_mask = ~np.isnan(tensor)
+        tensor[~tensor_mask] = 0
+
+    # Or impute nans with mean value across cell dimension (mean for each layer x gene)
+    elif fillna == "mean":
+        for i, layer in enumerate(tensor):
+            imputed = np.where(
+                np.isnan(layer), np.nanmean(layer, axis=0), layer
+            )
+            tensor[i] = imputed
+
+    if isinstance(ranks, int):
+        ranks = [ranks]
+
+    # Use gpu if available
+    tensor = tl.tensor(tensor)
+    try:
+        import torch
+
+        tl.set_backend("pytorch")
+        tensor = tl.tensor(tensor)
+
+        if (
+            (device == "auto") or (device == "cuda")
+        ) and torch.cuda.is_available():
+            device = "cuda"
+            tensor = tensor.to("cuda")
+        else:
+            device = "cpu"
+
+    except ImportError:
+        device = "cpu"
+
+    errors = []
+    for rank in tqdm(ranks, desc=f"Device {device}"):
+        for i in range(nruns):
+            # non-negative parafac decomposition
+            weights, factors = non_negative_parafac(
+                tensor, rank, init="random", random_state=random_state
+            )
+            
+            if device == "cuda":
+                weights = weights.cpu()
+                factors = [f.cpu() for f in factors]
+                tensor = tensor.cpu()
+
+            # calculate error ignoring missing values
+            tensor_mu = tl.cp_to_tensor((weights, factors))
+            error = np.sqrt(
+                (tensor[tensor != 0] - tensor_mu[tensor != 0]) ** 2
+            ).mean().numpy()
+            errors.append([error, rank])
+
+        # Save loadings from last decomposition run to adata
+        tensor_loadings = {}
+        sig_names = [f"Signature {i+1}" for i in range(factors[0].shape[1])]
+        adata.uns[f"r{rank}_signatures"] = pd.DataFrame(
+            factors[0].numpy(), index=layers, columns=sig_names
+        )
+        adata.obsm[f"r{rank}_signatures"] = pd.DataFrame(
+            factors[1].numpy(), index=adata.obs_names, columns=sig_names
+        )
+        adata.varm[f"r{rank}_signatures"] = pd.DataFrame(
+            factors[2].numpy(), index=adata.var_names, columns=sig_names
+        )
+
+    errors = pd.DataFrame(
+        errors, columns=["rmse", "rank"]
     )
-
-    tensor_loadings = {}
-    for layer, df in tensor_c2c.factors.items():
-        df.columns = df.columns.str.replace("Factor", "Signature")
-        tensor_loadings[layer] = df
-
-    adata.uns["tensor_loadings"] = tensor_loadings
-
-    _assign_factors(data)
+    errors["rmse"] = errors["rmse"].astype(float)
+    adata.uns["signatures_error"] = errors
 
     return adata
 
 
-def _assign_factors(data, n_clusters=None, copy=False):
-    adata = data.copy() if copy else data
+def lp_signatures(data, ranks, nruns=3, device="auto", random_state=888, copy=False):
+    """Calculate localization signatures by performing tensor decomposition on the dataset tensor.
+        Wrapper for `bento.tl.decompose_tensor()`.
 
-    # Get tensor dimension names
-    dim_names = list(adata.uns["tensor_labels"].keys())
-    cell_load = adata.uns["tensor_loadings"][dim_names[1]]
-    gene_load = adata.uns["tensor_loadings"][dim_names[2]]
+    Parameters
+    ----------
+    data : AnnData
+        Spatial formatted AnnData.
+    ranks : int
+        Rank(s) to perform decomposition.
+    device : str, optional
+        Type of device to use, valid options include `cpu`, `gpu`, and `auto`, by default `auto`.
+        Option `auto` prefers `gpu` over `cpu` if available.
+    random_state : int, optional
+        Random seed used for reproducibility, by default 888.
+    copy : bool
+        Return a copy of `data` instead of writing to data, by default False.
 
-    # If 1 component decomposition, don't cluster later
-    cluster_factors = True
-    if cell_load.shape[1] == 1:
-        cluster_factors = False
-
-    # Zscale for clustering
-    cell_load = pd.DataFrame(
-        StandardScaler().fit_transform(cell_load),
-        index=cell_load.index,
-        columns=cell_load.columns,
-    )
-    gene_load = pd.DataFrame(
-        StandardScaler().fit_transform(gene_load),
-        index=gene_load.index,
-        columns=gene_load.columns,
-    )
-
-    # Get sorted cell order from clustermap
-
-    iorder = sns.clustermap(
-        cell_load.T, row_cluster=cluster_factors, cmap="RdBu_r", center=0
-    ).dendrogram_col.reordered_ind
-    plt.close()
-
-    # Reorder cell names
-    iorder = pd.Series(
-        range(len(cell_load)), index=cell_load.index[iorder], name="td_cluster"
-    )
-    cell_to_factor = cell_load.join(iorder)["td_cluster"].tolist()
-
-    # Save associated tensor decomposition factor to adata.obs
-    adata.obs["td_cluster"] = cell_to_factor
-
-    # Get sorted cell order from clustermap
-    iorder = sns.clustermap(
-        gene_load.T, row_cluster=cluster_factors, cmap="RdBu_r", center=0
-    ).dendrogram_col.reordered_ind
-    plt.close()
-
-    # Reorder cell names
-    iorder = pd.Series(
-        range(len(gene_load)), index=gene_load.index[iorder], name="td_cluster"
-    )
-    gene_to_factor = gene_load.join(iorder)["td_cluster"].tolist()
-
-    # Save associated tensor decomposition factor to adata.var
-    adata.var["td_cluster"] = gene_to_factor
-    return adata
-
-
-def init_c2c_tensor(data, device="auto"):
-
-    if device != "cpu":
-        try:
-            import torch
-
-            device = "cuda" if torch.cuda.is_available() else "cpu"
-        except ImportError:
-            device = None
-
-    print(f"Device: {device}")
-
-    order_labels = list(data.uns["tensor_labels"].keys())
-    order_names = list(data.uns["tensor_labels"].values())
-
-    tensor_c2c = c2c.tensor.PreBuiltTensor(
-        data.uns["tensor"],
-        order_names=order_names,
-        order_labels=order_labels,
+    Returns
+    -------
+    _type_
+        _description_
+    """
+    return decompose_tensor(
+        data,
+        PATTERN_PROBS,
+        ranks,
         device=device,
+        random_state=random_state,
+        copy=copy,
     )
-
-    return tensor_c2c
