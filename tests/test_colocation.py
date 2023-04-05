2325a66702d34d2971aeb3f4a36ba663b4cd9a30
Clarence Mah <clarence.iyan@gmail.com>
2023-04-04 17:18:17 -0700

    plotting tests

--- tests/test_colocation.py
+++ tests/test_colocation.py
@@ -1,15 +1,21 @@
 import unittest
-import bento
+import bento as bt
 
-data = bento.datasets.sample_data()
+data = bt.datasets.sample_data()
+
+rank = 3
 
 
 class TestColocation(unittest.TestCase):
     def test_coloc_quotient(self):
-        bento.tl.coloc_quotient(data)
+        bt.tl.coloc_quotient(data)
         self.assertTrue("clq" in data.uns)
 
     def test_colocation(self):
-        bento.tl.coloc_quotient(data, radius=20, min_points=10, min_cells=0)
-        bento.tl.colocation(data, ranks=[3], iterations=3)
+        bt.tl.coloc_quotient(data, radius=20, min_points=10, min_cells=0)
+        bt.tl.colocation(data, ranks=[rank], iterations=3)
         self.assertTrue("clq" in data.uns)
+
+    def test_plot(self):
+        bt.pl.colocation(data, rank=rank)
+        self.assertTrue(True)
--- tests/test_lp.py
+++ tests/test_lp.py
@@ -10,3 +10,9 @@ class TestPatterns(unittest.TestCase):
 
         # Check if "lp" and "lpp" are in data.obsm
         self.assertTrue("lp" in data.uns.keys() and "lpp" in data.uns.keys())
+
+    def test_lp_plots(self):
+        bt.pl.lp_dist(data, percentage=True)
+        bt.pl.lp_dist(data, percentage=False)
+        bt.tl.lp_stats(data)
+        bt.pl.lp_genes(data)
