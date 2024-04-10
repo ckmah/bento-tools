53605595510dad59e387d955908477b034a2a1cf
Dylan Lam <dylanclam12@gmail.com>
2024-04-10 09:28:04 -0700

    _plotting.py Tests
    Tests for _plotting.py

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -1,105 +1,193 @@
-# import unittest
-# import bento as bt
-# import matplotlib as mpl
-# import matplotlib.pyplot as plt
-
-# adata = bt.ds.sample_data()
+import unittest
+import bento as bt
+import spatialdata as sd
+import matplotlib.pyplot as plt
 
 
-# # Test if plotting functions run without error
-# class TestPlotting(unittest.TestCase):
-#     def test_analyze(self):
-#         bt.pl.points(adata)
+# Test if plotting functions run without error
+class TestPlotting(unittest.TestCase):
+    def setUp(self):
+        datadir = "/".join(bt.__file__.split("/")[:-1]) + "/datasets"
+        self.imgdir = "/".join(bt.__file__.split("/")[:-2]) + "/tests/img/"
+        self.data = sd.read_zarr(f"{datadir}/small_data.zarr")
+        self.data = bt.io.format_sdata(
+            sdata=self.data,
+            points_key="transcripts",
+            feature_key="feature_name",
+            instance_key="cell_boundaries",
+            shape_keys=["cell_boundaries", "nucleus_boundaries"],
+        )
 
-#         bt.pl.points(adata, hue="gene", legend=False)
+    def test_points_plotting(self):
+        bt.pl.points(
+            self.data, 
+            fname=f"{self.imgdir}points/points_uncolored_synced"
+        )
+        plt.figure()
 
-#         genes = ["MALAT1", "TLN1", "SPTBN1"]
-#         bt.pl.points(adata[:, genes], hue="gene")
+        bt.pl.points(
+            self.data, 
+            sync_points=False, 
+            fname=f"{self.imgdir}points/points_uncolored_unsynced"
+        )
+        plt.figure()
 
-#         bt.pl.density(adata)
+        bt.pl.points(
+            self.data, 
+            hue="feature_name", 
+            legend=False, 
+            fname=f"{self.imgdir}points/points_colored_synced"
+        )
+        plt.figure()
 
-#         bt.pl.density(adata, kind="kde")
+        bt.pl.points(
+            self.data, 
+            hue="feature_name", 
+            legend=False, 
+            sync_points=False, 
+            fname=f"{self.imgdir}points/points_colored_unsynced"
+        )
+        plt.figure()
 
-#         bt.pl.shapes(adata)
+        genes = ["LUM", "POSTN", "CCDC80"]
+        bt.pl.points(
+            self.data,
+            hue="feature_name", 
+            hue_order=genes, 
+            legend=False, 
+            fname=f"{self.imgdir}points/points_subseted_genes_synced"
+        )
+        plt.figure()
 
-#         bt.pl.shapes(adata, color_style="fill")
+        bt.pl.points(
+            self.data, 
+            hue="feature_name", 
+            hue_order=genes, 
+            legend=False, 
+            sync_points=False, 
+            fname=f"{self.imgdir}points/points_subseted_genes_unsynced"
+        )
+        plt.figure()
 
-#         bt.pl.shapes(adata, hue="cell", color_style="fill")
+    def test_density_plotting(self):
+        bt.pl.density(
+            self.data, 
+            fname=f"{self.imgdir}density/density_hist"
+        )
+        plt.figure()
 
-#         fig, ax = plt.subplots()
-#         bt.pl.shapes(adata, shapes="cell", linestyle="--", ax=ax)
-#         bt.pl.shapes(
-#             adata,
-#             shapes="nucleus",
-#             edgecolor="black",
-#             facecolor="lightseagreen",
-#             ax=ax,
-#         )
-#         fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+        bt.pl.density(
+            self.data, 
+            kind="kde", 
+            fname=f"{self.imgdir}density/density_kde"
+        )
+        plt.figure()
 
-#         bt.pl.density(adata, ax=axes[0], title="default styling")
+        bt.pl.density(
+            self.data, 
+            hue="feature_name", 
+            legend=False, 
+            fname=f"{self.imgdir}density/density_hist_gene"
+        )
+        plt.figure()
 
-#         bt.pl.density(
-#             adata,
-#             ax=axes[1],
-#             axis_visible=True,
-#             frame_visible=True,
-#             square=True,
-#             title="square plot + axis",
-#         )
-#         plt.tight_layout()
-#         with mpl.style.context("dark_background"):
-#             fig, ax = plt.subplots()
-#             bt.pl.shapes(adata, shapes="cell", linestyle="--", ax=ax)
-#             bt.pl.shapes(
-#                 adata,
-#                 shapes="nucleus",
-#                 edgecolor="black",
-#                 facecolor="lightseagreen",
-#                 ax=ax,
-#             )
-#         cells = adata.obs_names[:8]  # get some cells
-#         ncells = len(cells)
+        bt.pl.density(
+            self.data, 
+            hue="feature_name", 
+            legend=False, 
+            kind="kde", 
+            fname=f"{self.imgdir}density/density_kde_gene"
+        )
+        plt.figure()
 
-#         ncols = 4
-#         nrows = 2
-#         ax_height = 1.5
-#         fig, axes = plt.subplots(
-#             nrows, ncols, figsize=(ncols * ax_height, nrows * ax_height)
-#         )  # instantiate
+        genes = ["LUM", "POSTN", "CCDC80"]
+        bt.pl.density(
+            self.data, 
+            hue="feature_name", 
+            hue_order=genes, 
+            legend=False, 
+            fname=f"{self.imgdir}density/density_hist_subsetted_genes"
+        )
+        plt.figure()
+        
+        bt.pl.density(
+            self.data, 
+            hue="feature_name", 
+            hue_order=genes, 
+            legend=False, 
+            kind="kde", 
+            fname=f"{self.imgdir}density/density_kde_subsetted_genes"
+        )
+        plt.figure()
 
-#         for c, ax in zip(cells, axes.flat):
-#             bt.pl.density(
-#                 adata[c],
-#                 ax=ax,
-#                 square=True,
-#                 title="",
-#             )
+        fig, axes = plt.subplots(1, 2, figsize=(8, 4))
+        bt.pl.density(self.data, ax=axes[0], title="default styling")
+        bt.pl.density(
+            self.data,
+            ax=axes[1],
+            axis_visible=True,
+            frame_visible=True,
+            square=True,
+            title="square plot + axis",
+            fname=f"{self.imgdir}density/density_square",
+        )
+        plt.tight_layout()
+        plt.figure()
 
-#         plt.subplots_adjust(wspace=0, hspace=0, bottom=0, top=1, left=0, right=1)
-#         batches = adata.obs["batch"].unique()[:6]  # get 6 batches
-#         nbatches = len(batches)
+    def test_shapes_plotting(self):
+        bt.pl.shapes(
+            self.data, 
+            fname=f"{self.imgdir}shapes/shapes_uncolored_synced"
+        )
+        plt.figure()
 
-#         ncols = 3
-#         nrows = 2
-#         ax_height = 3
-#         fig, axes = plt.subplots(
-#             nrows, ncols, figsize=(ncols * ax_height, nrows * ax_height)
-#         )  # instantiate
+        bt.pl.shapes(
+            self.data,
+            sync_shapes=False, 
+            fname=f"{self.imgdir}shapes/shapes_uncolored_unsynced"
+        )
+        plt.figure()
 
-#         for b, ax in zip(batches, axes.flat):
-#             bt.pl.density(
-#                 adata,
-#                 batch=b,
-#                 ax=ax,
-#                 square=True,
-#                 title="",
-#             )
+        bt.pl.shapes(
+            self.data, 
+            color_style="fill", 
+            fname=f"{self.imgdir}shapes/shapes_colored_synced"
+        )
+        plt.figure()
 
-#         # remove empty axes
-#         for ax in axes.flat[nbatches:]:
-#             ax.remove()
+        bt.pl.shapes(
+            self.data, 
+            color_style="fill",
+            sync_shapes=False, 
+            fname=f"{self.imgdir}shapes/shapes_colored_unsynced"
+        )
+        plt.figure()
 
-#         plt.subplots_adjust(wspace=0, hspace=0, bottom=0, top=1, left=0, right=1)
+        bt.pl.shapes(
+            self.data, 
+            hue="cell",
+            color_style="fill",
+            fname=f"{self.imgdir}shapes/shapes_cell_colored_synced"
+        )
+        plt.figure()
 
-#         self.assertTrue(True)
+        bt.pl.shapes(
+            self.data, 
+            hue="cell",
+            color_style="fill",
+            sync_shapes=False, 
+            fname=f"{self.imgdir}shapes/shapes_cell_colored_unsynced"
+        )
+        plt.figure()
+        
+        fig, ax = plt.subplots()
+        bt.pl.shapes(self.data, shapes="cell_boundaries", linestyle="--", ax=ax)
+        bt.pl.shapes(
+            self.data,
+            shapes="nucleus_boundaries",
+            edgecolor="black",
+            facecolor="lightseagreen",
+            ax=ax,
+            fname=f"{self.imgdir}shapes/shapes_nucleus_colored_synced"
+        )
+    
