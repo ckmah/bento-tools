7ec0cae56bcf9ee7e84a099a333f01a4c87a6ca8
ckmah <clarence.k.mah@gmail.com>
2024-04-22 09:52:30 -0700

    add sample data and read_zarr

--- bento/__init__.py
+++ bento/__init__.py
@@ -2,6 +2,7 @@ from . import _utils as ut
 from . import geometry as geo
 from . import plotting as pl
 from . import tools as tl
+from . import datasets as ds
 from . import io
 from .io import prep
 from .plotting import _colors as colors
