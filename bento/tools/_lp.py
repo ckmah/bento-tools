1171a4c81ffb31a4357f9a6aa97128706a26856e
ckmah <clarence.k.mah@gmail.com>
2024-05-02 17:34:35 -0700

    lp parallelized

--- bento/tools/_lp.py
+++ bento/tools/_lp.py
@@ -18,11 +18,14 @@ from .._constants import PATTERN_NAMES
 
 tqdm.pandas()
 
+
 def lp(
-    sdata: SpatialData, 
+    sdata: SpatialData,
     instance_key: str = "cell_boundaries",
     nucleus_key: str = "nucleus_boundaries",
-    groupby: Optional[Union[str, List[str]]] = "feature_name"
+    groupby: str | List[str] = "feature_name",
+    num_workers=1,
+    recompute=False,
 ):
     """Predict transcript subcellular localization patterns.
     Patterns include: cell edge, cytoplasmic, nuclear edge, nuclear, none
@@ -32,7 +35,7 @@ def lp(
     sdata : SpatialData
         Spatial formatted SpatialData object
 
-    groupby : str or list of str, optional (default: None)
+    groupby : str or list of str
         Key in `sdata.points[points_key] to groupby, by default None. Always treats each cell separately
 
     Returns
@@ -47,7 +50,7 @@ def lp(
     if isinstance(groupby, str):
         groupby = [groupby]
 
-    pattern_features = [ # Do not change order of features!
+    pattern_features = [  # Do not change order of features!
         f"{instance_key}_inner_proximity",
         f"{nucleus_key}_inner_proximity",
         f"{nucleus_key}_outer_proximity",
@@ -63,25 +66,28 @@ def lp(
         f"{nucleus_key}_dispersion_norm",
     ]
 
-
     # Compute features
     feature_key = f"{instance_key}_{'_'.join(groupby)}_features"
-    if feature_key not in sdata.table.uns.keys() or not all(
-        f in sdata.table.uns[feature_key].columns for f in pattern_features
+    if (
+        feature_key not in sdata.table.uns.keys()
+        or not all(f in sdata.table.uns[feature_key].columns for f in pattern_features)
+        or recompute
     ):
         bento.tl.analyze_points(
             sdata,
             instance_key,
             ["proximity", "asymmetry", "ripley", "point_dispersion_norm"],
             groupby=groupby,
-            recompute=False,
+            recompute=recompute,
+            num_workers=num_workers,
         )
         bento.tl.analyze_points(
             sdata,
             nucleus_key,
             ["proximity", "asymmetry", "shape_dispersion_norm"],
             groupby=groupby,
-            recompute=False,
+            recompute=recompute,
+            num_workers=num_workers,
         )
 
     X_df = sdata.table.uns[feature_key][pattern_features]
@@ -104,7 +110,9 @@ def lp(
     )
 
     # Add cell and groupby identifiers
-    pattern_prob.index = sdata.table.uns[feature_key].set_index([instance_key, *groupby]).index
+    pattern_prob.index = (
+        sdata.table.uns[feature_key].set_index([instance_key, *groupby]).index
+    )
 
     # Set to no class if sample had nan feature values
     pattern_prob.loc[:, invalid_samples] = 0
@@ -115,6 +123,7 @@ def lp(
 
     sdata.table.uns["lp"] = indicator_df.reset_index()
     sdata.table.uns["lpp"] = pattern_prob.reset_index()
+
 
 def lp_stats(sdata: SpatialData, instance_key: str = "cell_boundaries"):
     """Computes frequencies of localization patterns across cells and genes.
@@ -137,8 +146,11 @@ def lp_stats(sdata: SpatialData, instance_key: str = "cell_boundaries"):
     groupby = list(cols[~cols.isin(PATTERN_NAMES)])
     groupby.remove(instance_key)
 
-    g_pattern_counts = lp.groupby(groupby).apply(lambda df: df[PATTERN_NAMES].sum().astype(int))
+    g_pattern_counts = lp.groupby(groupby).apply(
+        lambda df: df[PATTERN_NAMES].sum().astype(int)
+    )
     sdata.table.uns["lp_stats"] = g_pattern_counts
+
 
 def _lp_logfc(sdata, instance_key, phenotype=None):
     """Compute pairwise log2 fold change of patterns between groups in phenotype.
@@ -214,6 +226,7 @@ def _lp_logfc(sdata, instance_key, phenotype=None):
 
     return gene_fc_stats
 
+
 def _lp_diff_gene(cell_by_pattern, phenotype_series, instance_key):
     """Perform pairwise comparison between groupby and every class.
 
@@ -236,7 +249,9 @@ def _lp_diff_gene(cell_by_pattern, phenotype_series, instance_key):
     # One hot encode categories
     group_dummies = pd.get_dummies(phenotype_series)
     group_names = group_dummies.columns.tolist()
-    group_data = cell_by_pattern.set_index(instance_key).join(group_dummies, how='inner')
+    group_data = cell_by_pattern.set_index(instance_key).join(
+        group_dummies, how="inner"
+    )
     group_data.columns = group_data.columns.astype(str)
 
     # Perform one group vs rest logistic regression
@@ -268,18 +283,17 @@ def _lp_diff_gene(cell_by_pattern, phenotype_series, instance_key):
             ValueError,
             PerfectSeparationError,
             PatsyError,
-        ) as e:
-            continue
+        ):
+            pass
 
     if len(results) > 0:
         results = pd.concat(results)
 
     return results if len(results) > 0 else None
 
+
 def lp_diff_discrete(
-    sdata: SpatialData,
-    instance_key: str = "cell_boundaries", 
-    phenotype: str = None
+    sdata: SpatialData, instance_key: str = "cell_boundaries", phenotype: str = None
 ):
     """Gene-wise test for differential localization across phenotype of interest.
 
@@ -304,7 +318,9 @@ def lp_diff_discrete(
     # Retrieve cell phenotype
     phenotype_series = sdata.shapes[instance_key][phenotype]
     if is_numeric_dtype(phenotype_series):
-        raise KeyError(f"Phenotype dtype must not be numeric | dtype: {phenotype_series.dtype}")
+        raise KeyError(
+            f"Phenotype dtype must not be numeric | dtype: {phenotype_series.dtype}"
+        )
 
     # [Sample by patterns] where sample id = [cell, group] pair
     pattern_df = sdata.table.uns["lp"].copy()
@@ -326,8 +342,12 @@ def lp_diff_discrete(
     results["-log10padj"] = -np.log10(results["padj"].astype(np.float32))
 
     # Cap significance values
-    results.loc[results["-log10p"] == np.inf, "-log10p"] = results.loc[results["-log10p"] != np.inf]["-log10p"].max()
-    results.loc[results["-log10padj"] == np.inf, "-log10padj"] = results.loc[results["-log10padj"] != np.inf]["-log10padj"].max()
+    results.loc[results["-log10p"] == np.inf, "-log10p"] = results.loc[
+        results["-log10p"] != np.inf
+    ]["-log10p"].max()
+    results.loc[results["-log10padj"] == np.inf, "-log10padj"] = results.loc[
+        results["-log10padj"] != np.inf
+    ]["-log10padj"].max()
 
     # Group-wise log2 fold change values
     log2fc_stats = _lp_logfc(sdata, instance_key, phenotype)
@@ -345,10 +365,9 @@ def lp_diff_discrete(
     # Save back to SpatialData
     sdata.table.uns[f"diff_{phenotype}"] = results
 
+
 def lp_diff_continuous(
-    sdata: SpatialData,
-    instance_key: str = "cell_boundaries",
-    phenotype: str = None
+    sdata: SpatialData, instance_key: str = "cell_boundaries", phenotype: str = None
 ):
     """Gene-wise test for differential localization across phenotype of interest.
 
@@ -372,7 +391,6 @@ def lp_diff_continuous(
     # Retrieve cell phenotype
     phenotype_series = sdata.shapes[instance_key][phenotype]
 
-
     pattern_dfs = {}
     # Compute correlation for each point group along cells
     for p in PATTERN_NAMES:
@@ -380,17 +398,17 @@ def lp_diff_continuous(
         p_labels = lpp.pivot(index=instance_key, columns=groups_name, values=p)
         p_corr = p_labels.corrwith(phenotype_series, axis=0, drop=True)
 
-        pattern_df = pd.DataFrame(p_corr).reset_index(drop = False)
-        pattern_df.insert(loc = 1, column = 'pattern', value = p)
-        pattern_df = pattern_df.rename(columns = {0:'pearson_correlation'})
+        pattern_df = pd.DataFrame(p_corr).reset_index(drop=False)
+        pattern_df.insert(loc=1, column="pattern", value=p)
+        pattern_df = pattern_df.rename(columns={0: "pearson_correlation"})
         pattern_dfs[p] = pattern_df
-    
+
     # Concatenate all pattern_dfs into one
     pattern_dfs = (
         pd.concat(pattern_dfs.values(), ignore_index=True)
-        .sort_values(by=['pearson_correlation'], ascending=False)
+        .sort_values(by=["pearson_correlation"], ascending=False)
         .reset_index(drop=True)
-        )
-        
-    pattern_dfs = pattern_dfs.loc[~pattern_dfs['pearson_correlation'].isna()]
+    )
+
+    pattern_dfs = pattern_dfs.loc[~pattern_dfs["pearson_correlation"].isna()]
     sdata.table.uns[f"diff_{phenotype}"] = pattern_dfs
