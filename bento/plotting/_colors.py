006a268a483fe4aec0034429245f2becf2433218
ckmah <clarence.k.mah@gmail.com>
2024-06-04 16:54:47 -0700

    compat for mpl

--- bento/plotting/_colors.py
+++ bento/plotting/_colors.py
@@ -10,8 +10,8 @@ red2blue_dark = LinearSegmentedColormap.from_list(
     "red2blue_dark", [blue_rgb, "black", red_rgb]
 )
 
-mpl.cm.register_cmap("red2blue", red2blue)
-mpl.cm.register_cmap("red2blue_dark", red2blue_dark)
+mpl.cm.register("red2blue", red2blue)
+mpl.cm.register("red2blue_dark", red2blue_dark)
 
 red_light = sns.light_palette(red_rgb, as_cmap=True)
 blue_light = sns.light_palette(blue_rgb, as_cmap=True)
