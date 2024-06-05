b3ba7496e948698836999e36e4cbe0e0d095b708
ckmah <clarence.k.mah@gmail.com>
2024-06-04 17:06:59 -0700

    mpl register fix

--- bento/plotting/_colors.py
+++ bento/plotting/_colors.py
@@ -10,8 +10,8 @@ red2blue_dark = LinearSegmentedColormap.from_list(
     "red2blue_dark", [blue_rgb, "black", red_rgb]
 )
 
-mpl.cm.register("red2blue", red2blue)
-mpl.cm.register("red2blue_dark", red2blue_dark)
+mpl.colormaps.register(red2blue, name="red2blue")
+mpl.colormaps.register(red2blue_dark, name="red2blue_dark")
 
 red_light = sns.light_palette(red_rgb, as_cmap=True)
 blue_light = sns.light_palette(blue_rgb, as_cmap=True)
